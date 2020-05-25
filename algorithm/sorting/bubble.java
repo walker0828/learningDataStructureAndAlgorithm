481f08ae25f281a1187c63f906b92deb2907203c
yachao.ni <yachao.ni@ipinyou.com>
2020-05-25 19:58:03 +0800

    first commit

---
+++ algorithm/sorting/bubble.java
@@ -0,0 +1,9 @@
+class solution {
+    public void bubble(String s){
+
+    }
+
+    public static void main(String[] args){
+        
+    }
+}
---
+++ algorithm/sorting/insert.java
@@ -0,0 +1,5 @@
+package algorithm.sorting;
+
+public class insert {
+    
+}
