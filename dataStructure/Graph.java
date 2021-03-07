2ffca3be5cdfb9b00711311b0250f89a5a15b9c0
yachao.ni <yachao.ni@ipinyou.com>
2021-03-07 18:56:12 +0800

    13

--- dataStructure/Graph.java
+++ dataStructure/Graph.java
@@ -1,8 +1,38 @@
-public class Graph {
-    private int vertexSize;
-    private int[] vertexs;
-    private int[][] matrix;
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    
+public class Graph {
+
+    private int value;
+    private LinkedList<Integer> adj[];
+
+    public Graph(int value) {
+        this.value = value;
+        adj = new LinkedList[value];
+        for(int i =0; i < value; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int s, int t) {
+        adj[s].add(t);
+        adj[t].add(s);
+    }
+
+    public void bfs(int s, int t) {
+        if(s==t) return;
+        boolean[] visited = new boolean[v];
+        visited[s] = true;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+
+    }
+
+    public void dfs(int s, int t) {
+
+    }
+
 }
 
